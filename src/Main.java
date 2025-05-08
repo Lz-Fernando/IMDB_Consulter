e3afac759a2b058d83d914ca9a2529d700479fd0
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-05-08 20:50:01 -0300

    Trabalhar com JSON, Strings e Regex

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,41 @@ package src;
 
 import java.io.IOException;
 
+import java.lang.reflect.Type;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        String apiKey = carregarApiKey();
+        String json = buascarDadosDaApi(apiKey);
+        List<Filme> filmes = converterjsonParaLista(json);
+
+        exibirFilmes(filmes);
+    }
+
+
+    private static String carregarApiKey() {
         Dotenv dotenv = Dotenv.load();
         String apiKey = dotenv.get("RAPIDAPI_KEY");
+
+        return apiKey;
+    }
+
+    private static String buascarDadosDaApi(String apiKey) {
+        String json;
 
         try {
             HttpRequest request = HttpRequest.newBuilder()
@@ -25,8 +47,27 @@ public class Main {
                     .build();
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println(response.body());
+            json = response.body();
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
+        }
+
+        return json;
+    }
+
+    private static List<Filme> converterjsonParaLista(String json) {
+        Gson gson = new Gson();
+
+        Type tipoLista = new TypeToken<List<Filme>>() {
+        }.getType();
+        List<Filme> filmes = gson.fromJson(json, tipoLista);
+
+        return filmes;
+    }
+
+    private static void exibirFilmes(List<Filme> filmes) {
+        for (Filme f : filmes) {
+            System.out.println(f.getTitle());
         }
     }
 }
